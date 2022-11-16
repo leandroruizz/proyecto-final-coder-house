500fd2131b3b6aef77ee0c80e9f9205a3174e57e
leandro ruiz <leandrolmedo2000@gmail.com>
2022-11-16 14:39:17 -0300

    se corrigieron errores

--- enviar.php
+++ enviar.php
@@ -1,10 +1,10 @@
 <?php
-$destino = "purenightcycle@gmail.com"
-$nombre = $_POST["nombre"];
-$correo = $_POST["correo"];
-$instagram = $_POST["instagram"];
-$mensaje = $_POST["mensaje"];
-$contenido = "nombre: " . $nombre . "\ncorreo: " . $correo ."\ninstagram:" . $instagram . "\nmensaje: " . $mensaje;
-mail($destino,"contacto", $contenido);
-header("location:enviado.html")
+   $destino = "purenightcycle@gmail.com";
+   $nombre = $_POST["nombre"];
+   $correo = $_POST["correo"];
+   $instagram = $_POST["instagram"];
+   $mensaje = $_POST["mensaje"];
+   $contenido = "nombre: " . $nombre . "\ncorreo: " . $correo ."\ninstagram:" . $instagram . "\nmensaje: " . $mensaje;
+   mail($destino,"contacto", $contenido);
+   header("location:enviado.html");
 ?>
